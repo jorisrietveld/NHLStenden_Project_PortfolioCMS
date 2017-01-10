14f7a027f16b161f51c911e70438c3f67c1cfd03
Aron Soppe <aronsoppe@hotmail.com>
2017-01-10 11:35:19 +0100

    css in site folder

--- src/PortfolioCMS/Themes/site/home.php
+++ src/PortfolioCMS/Themes/site/home.php
@@ -18,7 +18,7 @@
     <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="../lib/cover.css" rel="stylesheet">
+    <link href="lib/cover.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
