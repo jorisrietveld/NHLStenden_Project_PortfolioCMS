ca25aa895432ad8cc596f1034f569686bf982094
Joris Rietveld <jorisrietveld@gmail.com>
2017-01-11 22:48:14 +0100

    Added doc about the student.

--- src/PortfolioCMS/Controller/SiteHelper.php
+++ src/PortfolioCMS/Controller/SiteHelper.php
@@ -17,7 +17,7 @@ trait SiteHelper
     /**
      * Render the base menu.
      */
-    public function renderMenuLinks(  )
+  /*  public function renderMenuLinks(  )
     {
         $menuLinks = '';
 
@@ -33,7 +33,39 @@ trait SiteHelper
             );
         }
         return $menuLinks;
-    }
+    }*/
+
+    /**
+     * Dummy method to show some fake data in the drop down.
+     * @return string
+     */
+    public function renderMenuLinks(  )
+      {
+          $menuLinks = '';
+
+          $fakePortfolios = [
+              'Aron soppe',
+              'Esmee Lunenborg',
+              'Anouk van der veen',
+              'Marco Brink',
+              'Kevin Tabak',
+              'Joris Rietveld',
+              'Kevin Veldman',
+          ];
+
+          foreach ( $fakePortfolios as $portfolio )
+          {
+              $menuLinks .= sprintf(
+                  '<li>
+                  <a href="#">
+                      <i class="fa fa-caret-right" aria-hidden="true"></i> %s
+                  </a>
+              </li>',
+                  $portfolio
+              );
+          }
+          return $menuLinks;
+      }
 
     protected function getPortfolios(  )
     {
