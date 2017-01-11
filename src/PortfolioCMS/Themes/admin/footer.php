8fd6acdb06802da108630f9309428093e4f0d428
Aron Soppe <aronsoppe@hotmail.com>
2017-01-11 14:28:13 +0100

    admin pages routing done, needs to be filled up with content (Aron en Marco)

---
+++ src/PortfolioCMS/Themes/admin/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="pull-left">
+            <ul>
+                <li>
+                    <a href="admin">
+                        Home
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <p class="copyright pull-right">
+            &copy; 2017 -- INF1B Periode 2
+        </p>
+    </div>
+</footer>
---
+++ src/PortfolioCMS/Themes/admin/thema.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<?php
+
+$page_title = "Thema | Admin";
+$isOnAdminPage = "thema";
+
+include 'header.php'; ?>
+<body>
+
+<?php include 'navigation.php' ?>
+
+<div class="content">
+    <div class="container-fluid">
+        thema verwijderen/toevoegen/bewerken
+    </div>
+</div>
+
+<?php include 'footer.php' ?>
+
+</body>
+
+<!--   Core JS Files   -->
+<script src="assets/admin/js/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="assets/admin/js/bootstrap.min.js" type="text/javascript"></script>
+
+<!--  Checkbox, Radio & Switch Plugins -->
+<script src="assets/admin/js/bootstrap-checkbox-radio-switch.js"></script>
+
+<!--  Charts Plugin -->
+<script src="assets/admin/js/chartist.min.js"></script>
+
+<!--  Notifications Plugin    -->
+<script src="assets/admin/js/bootstrap-notify.js"></script>
+
+<!--  Google Maps Plugin    -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false"></script>
+
+<!-- Navbar scripting -->
+<script src="assets/admin/js/light-bootstrap-dashboard.js"></script>
+
+</html>
