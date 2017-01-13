b583892a051a82e3afed2833f04c73a6221d97f0
Marco Brink <marcobrink@outlook.com>
2017-01-13 12:21:27 +0100

    no message

--- src/PortfolioCMS/Themes/Theme_marco/index.php
+++ src/PortfolioCMS/Themes/Theme_marco/index.php
@@ -12,13 +12,13 @@
     <title>Portfolio Marco Brink</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?= $dataProvider->get("request-uri") ?>assets/Theme_marco/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Theme CSS -->
     <link href="<?= $dataProvider->get("request-uri") ?>assets/Theme_marco/css/styles_1.css" rel="stylesheet">
 
     <!-- Custom Fonts -->
-    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="<?= $dataProvider->get("request-uri") ?>assets/Theme_marco/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Wellfleet" rel="stylesheet">
