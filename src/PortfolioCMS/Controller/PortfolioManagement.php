8158289c16830e8655b3dce4aecf433806706f81
Joris Rietveld <jorisrietveld@gmail.com>
2017-01-13 18:17:25 +0100

    Added sql install scripts

--- src/PortfolioCMS/Controller/PortfolioManagement.php
+++ src/PortfolioCMS/Controller/PortfolioManagement.php
@@ -10,11 +10,64 @@ namespace StendenINF1B\PortfolioCMS\Controller;
 
 
 use StendenINF1B\PortfolioCMS\Kernel\BaseController;
+use StendenINF1B\PortfolioCMS\Kernel\Database\Entity\Portfolio;
 use StendenINF1B\PortfolioCMS\Kernel\Http\Request;
+use StendenINF1B\PortfolioCMS\Kernel\Http\Response;
 
 class PortfolioManagement extends BaseController 
 {
+    protected $requiredInsertFields = [
+        'title',
+        'url',
+        'themeId',
+        'userId',
+    ];
+
+
     public function index( Request $request )
+    {
+        
+    }
+
+    public function remove( Request $request )
+    {
+        
+    }
+
+    public function insert( Request $request )
+    {
+        $postParams = $request->getPostParams();
+
+        if( $this->checkPostParams( $postParams, $this->requiredInsertFields ))
+        {
+            $themeRepository = $this->getEntityManager()->getRepository( 'Theme' );
+            $studentRepository = $this->getEntityManager()->getRepository( 'Student' );
+            $portfolioRepository = $this->getEntityManager()->getRepository( 'Portfolio' );
+
+            $newPortfolio = new Portfolio();
+            $newPortfolio->setTitle( (string)$postParams->get( 'title') );
+            $newPortfolio->setUrl( (string)$postParams->get( 'url' ) );
+            $newPortfolio->setTheme( $themeRepository->getById( (int)$postParams->get( 'themeId' ) ) );
+            $newPortfolio->setStudent( $studentRepository->getById( (int)$postParams->get( 'userId' ) ) );
+
+            $portfolioRepository->insert( $newPortfolio );
+
+            return new Response(
+                $this->renderWebPage(
+                    'admin:editPortfolio', [
+                        'feedback' => 'Het porfolio is toegevoed.',
+                    ]
+                ),
+                Response::HTTP_STATUS_OK
+            );
+        }
+        else
+        {
+
+        }
+    }
+
+    public function edit( Request $request )
     {
         
     }
--- src/PortfolioCMS/Controller/UserManagement.php
+++ src/PortfolioCMS/Controller/UserManagement.php
@@ -11,49 +11,76 @@ namespace StendenINF1B\PortfolioCMS\Controller;
 
 use StendenINF1B\PortfolioCMS\Kernel\BaseController;
 use StendenINF1B\PortfolioCMS\Kernel\Database\Entity\Teacher;
+use StendenINF1B\PortfolioCMS\Kernel\Database\Entity\Student;
 use StendenINF1B\PortfolioCMS\Kernel\Http\Request;
 use StendenINF1B\PortfolioCMS\Kernel\Http\Response;
 
 class UserManagement extends BaseController 
 {
+    protected $requiredInsertUserFields = [
+        'password',
+        'email',
+        'firstName',
+        'lastName',
+        'isAdmin',
+    ];
+
+    protected $requiredInsertStudentFields = [
+        'address',
+        'zipCode',
+        'dateOfBirth',
+        'studentCode',
+        'phoneNumber',
+    ];
+
+    protected $requiredInsertTeacherFields = [
+        'isSLBer'
+    ];
+
     public function index( Request $request )
     {
         
     }
 
+    /**
+     * Inserts an new student into the database.
+     *
+     * @param Request $request
+     * @return Response
+     */
     public function insertStudent( Request $request )
     {
         $postParams = $request->getPostParams();
 
-        if ( $postParams->has( 'password' ) &&
-            $postParams->has( 'email' ) &&
-            $postParams->has( 'firstName' ) &&
-            $postParams->has( 'lastName' ) &&
-            $postParams->has( 'isAdmin' ) &&
-            $postParams->has( 'address' ) &&
-            $postParams->has( 'zipCode' ) &&
-            $postParams->has( 'location' ) &&
-            $postParams->has( 'dateOfBirth' ) &&
-            $postParams->has( 'studentCode' ) &&
-            $postParams->has( 'phoneNumber' )
-        )
+        if ( $this->checkPostParams( $postParams, array_merge( $this->requiredInsertUserFields, $this->requiredInsertStudentFields ) ) )
         {
-            $student = new Student();
-            $student->setHashedPassword( password_hash( $postParams->get( 'password' ), PASSWORD_BCRYPT ) );
-            $student->setEmail( (string)$postParams->get( 'email' ) );
-            $student->setLastIpAddress( $request->getClientIp() );
-            $student->setFirstName( (string)$postParams->get( 'firstName' ) );
-            $student->setLastName( (string)$postParams->get( 'lastName' ) );
-            $student->setIsAdmin( (bool)$postParams->get( 'isAdmin' ) );
-            $student->setAddress( (string)$postParams->get( 'address' ) );
-            $student->setZipCode( (string)$postParams->get( 'zipCode' ) );
-            $student->setLocation( (string)$postParams->get( 'location' ) );
-            $student->setDateOfBirth( new \DateTime( $postParams->get( 'dateOfBirth' ) ) );
-            $student->setStudentCode( (string)$postParams->get( 'studentCode' ) );
-            $student->setPhoneNumber( (string)$postParams->get( 'phoneNumber' ) );
-
             $studentRepo = $this->getEntityManager()->getRepository( 'Student' );
-            $studentRepo->insert( $student );
+            $newStudent = new Student();
+
+            $newStudent->setHashedPassword( password_hash( $postParams->get( 'password' ), PASSWORD_BCRYPT ) );
+            $newStudent->setEmail( (string)$postParams->get( 'email' ) );
+            $newStudent->setLastIpAddress( $request->getClientIp() );
+            $newStudent->setFirstName( (string)$postParams->get( 'firstName' ) );
+            $newStudent->setLastName( (string)$postParams->get( 'lastName' ) );
+            $newStudent->setIsAdmin( (bool)$postParams->get( 'isAdmin' ) );
+            $newStudent->setAddress( (string)$postParams->get( 'address' ) );
+            $newStudent->setZipCode( (string)$postParams->get( 'zipCode' ) );
+            $newStudent->setLocation( (string)$postParams->get( 'location' ) );
+            $newStudent->setDateOfBirth( new \DateTime( $postParams->get( 'dateOfBirth' ) ) );
+            $newStudent->setStudentCode( (string)$postParams->get( 'studentCode' ) );
+            $newStudent->setPhoneNumber( (string)$postParams->get( 'phoneNumber' ) );
+
+            // Insert the new student.
+            $studentRepo->insert( $newStudent );
+
+            return new Response(
+                $this->renderWebPage(
+                    '', [
+
+                    ]
+                )
+            );
+
         }
         else
         {
@@ -70,29 +97,26 @@ class UserManagement extends BaseController
     {
         $postParams =  $request->getPostParams();
 
-        if ( $postParams->has( 'password' ) &&
-            $postParams->has( 'email' ) &&
-            $postParams->has( 'firstName' ) &&
-            $postParams->has( 'lastName' ) &&
-            $postParams->has( 'isAdmin' ) &&
-            $postParams->has( 'isSlber' )
-        ){
-            $teacher =  new Teacher();
+        if ( $this->checkPostParams( $postParams, array_merge( $this->requiredInsertUserFields, $this->requiredInsertTeacherFields ) ) )
+        {
             $teacherRepo = $this->getEntityManager()->getRepository( 'Teacher' );
 
-            $teacher->setLastName( (string)$postParams->get( 'lastName' ) );
-            $teacher->setFirstName( (string)$postParams->get('firstName' ) );
-            $teacher->setEmail( (string)$postParams->get( 'email' ));
-            $teacher->setHashedPassword( password_hash( $postParams->get('password'), PASSWORD_BCRYPT ));
-            $teacher->setIsSLBer( (bool) $postParams->get('isSlber') );
-            $teacher->setIsAdmin( (bool)  $postParams->get( 'isAdmin' ) );
+            $newTeacher =  new Teacher();
+            $newTeacher->setLastName( (string)$postParams->get( 'lastName' ) );
+            $newTeacher->setFirstName( (string)$postParams->get('firstName' ) );
+            $newTeacher->setEmail( (string)$postParams->get( 'email' ));
+            $newTeacher->setHashedPassword( password_hash( $postParams->get('password'), PASSWORD_BCRYPT ));
+            $newTeacher->setIsSLBer( (bool) $postParams->get('isSlber') );
+            $newTeacher->setIsAdmin( (bool)  $postParams->get( 'isAdmin' ) );
 
-            $teacherRepo->insert( $teacher );
+            $teacherRepo->insert( $newTeacher );
 
             return new Response(
                 $this->renderWebPage(
-                    
-                );
+                    '', [
+
+                    ]
+                )
             );
         }
         else
